9817c8a10f13711dd636596daa6cb95987a2dcab
Chua Zong Ying (Josh) <zychua@nexify.com.hk>
2023-07-20 12:20:14 +0800

    End of section 5

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,25 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { EnviromentsComponent } from './enviroments/enviroments.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    EnviromentsComponent
+    EnviromentsComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -7,25 +9,35 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  model:any = {}
-  loggedIn = false;
-  constructor(private accountService: AccountService){}
+  model:any = {};
+  currentUser$: Observable<User | null> = of(null);
+
+  constructor(public accountService: AccountService){}
   
   ngOnInit(): void {
+    // this.currentUser$ = this.accountService.currentUser$;
 
   }
+
+  // getCurrentUser(){
+  //   this.accountService.currentUser$.subscribe({
+  //     next: user => this.loggedIn = !!user,
+  //     error: error => console.log(error)
+  //   })
+  // }
 
   login(){
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
+        // this.loggedIn = true;
       },
       error: error => console.log(error)
     })
   }
 
   logout(){
-    this.loggedIn = false;
+    this.accountService.logout();
+    // this.loggedIn = false;
   }
 }
