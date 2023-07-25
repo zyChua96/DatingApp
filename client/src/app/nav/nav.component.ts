50dc2111e29fa656e6bd3bea7311c37a095ddde4
Chua Zong Ying (Josh) <zychua@nexify.com.hk>
2023-07-25 16:15:59 +0800

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -21,8 +21,7 @@ export class NavComponent implements OnInit {
 
   login(){
     this.accountService.login(this.model).subscribe({
-      next: response => this.router.navigateByUrl('/members'),
-      error: error => this.toastr.error(error.error)
+      next: response => this.router.navigateByUrl('/members')
     })
   }
 
