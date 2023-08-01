b3768b3c8818c76d1e5ef95fa607bc2ba97e0a48
Chua Zong Ying (Josh) <zychua@nexify.com.hk>
2023-08-01 15:44:48 +0800

    End of section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.DataAnnotations;
 using API.Extensions;
 
 namespace API.Entities
@@ -21,8 +20,9 @@ namespace API.Entities
         public string Country { get; set; }
         public List<Photo> Photos { get; set; } = new();
 
-        public int GetAge(){
-            return DateOfBirth.CalculateAge();
-        }
+        // public int GetAge()
+        // {
+        //     return DateOfBirth.CalculateAge();
+        // }
     }
 }
