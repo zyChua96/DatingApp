dc0c1a4564922bea13cc42b26b2122c11531e5cd
Chua Zong Ying (Josh) <zychua@nexify.com.hk>
2023-08-14 09:56:07 +0800

    End of section 9

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -10,10 +10,10 @@ export class HomeComponent implements OnInit{
   registerMode =false;
   users: any;
 
-  constructor(private http: HttpClient){}
+  constructor(){}
 
   ngOnInit(): void {
-    this.getUsers();
+    // this.getUsers();
   }
 
   registerToggle(){
@@ -21,13 +21,13 @@ export class HomeComponent implements OnInit{
     
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete:() => console.log('Request has completed')
-    })
-  }
+  // getUsers(){
+  //   this.http.get('https://localhost:5001/api/users').subscribe({
+  //     next: response => this.users = response,
+  //     error: error => console.log(error),
+  //     complete:() => console.log('Request has completed')
+  //   })
+  // }
 
   cancelRegisterMode(event: boolean){
     this.registerMode = event;
