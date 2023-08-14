0accabba2098334e5f06019a68ac1c6d54827240
Chua Zong Ying (Josh) <zychua@nexify.com.hk>
2023-08-14 18:09:54 +0800

    End of section 10

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -8,19 +9,20 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit{
-  member: Member[] = [];
+  members$: Observable<Member[]> | undefined;
 
   constructor(private memberService:MembersService){
 
   }
 
   ngOnInit(): void {
-    this.loadMembers();
+    // this.loadMembers();
+    this.members$ = this.memberService.getMembers();
   }
 
-  loadMembers(){
-    this.memberService.getMembers().subscribe({
-      next: members=> this.member = members
-    })
-  }
+  // loadMembers(){
+  //   this.memberService.getMembers().subscribe({
+  //     next: members=> this.member = members
+  //   })
+  // }
 }
