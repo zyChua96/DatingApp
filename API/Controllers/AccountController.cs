a148ef2af21db49f385f08536c2336c99b463182
Chua Zong Ying (Josh) <zychua@nexify.com.hk>
2023-08-16 14:03:02 +0800

    End of section 11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -49,7 +49,7 @@ namespace API.Controllers
       [HttpPost("login")]
       public async Task<ActionResult<UserDto>> Login (LoginDto loginDto)
       {
-        var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+        var user = await _context.Users.Include(p =>p.Photos).SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
         if(user == null) return Unauthorized("Invalid Username");
 
 
@@ -63,7 +63,8 @@ namespace API.Controllers
         return new UserDto
         {
           Username = user.UserName,
-          Token = _tokenService.CreateToken(user)
+          Token = _tokenService.CreateToken(user),
+          PhotoUrl =user.Photos.FirstOrDefault(x => x.IsMain)?.Url
         };
 
       }
