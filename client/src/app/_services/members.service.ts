01f207df67e1d2159a1c44797f4abbd6873e12cb
Chua Zong Ying (Josh) <zychua@nexify.com.hk>
2023-08-23 10:26:32 +0800

    End of section 14

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -111,4 +111,18 @@ export class MembersService {
   deletePhoto(photoId: number){
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
+
+  addLike(username: string){
+    return this.http.post(this.baseUrl + 'likes/' + username, {});
+  }
+
+  getLikes(predicate: string, pageNumber: number, pageSize:number){
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+
+    params = params.append('predicate', predicate);
+    
+    // return this.http.get<Member[]>(this.baseUrl + 'likes?predicate=' + predicate);
+
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'likes', params);
+  }
 }
